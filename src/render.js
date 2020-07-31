4b29eb7f66090aa5b803acb8132d848d54c3f0e5
Ramashish Katiyar <65862890+spunkiee@users.noreply.github.com>
2020-07-31 23:16:59 +0530

    Update render.js

--- src/render.js
+++ src/render.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export const Render = () => {
+export const Render1 = () => {
   const [list, setList] = useState([1, 2, 3]);
   const [rerender, setRerender] = useState(false);
 
@@ -39,3 +39,55 @@ export const Render = () => {
 //    but react component is never going to knoe this because after pushing the list
 //    the previous list and the new list is same
 // 2. On click button this re render again, Now whole list render
+
+export class Render extends React.Component {
+  state = {
+    list: [1, 2, 3]
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.list !== this.state.list) {
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div>
+        {list.map((v, i) => {
+          return (
+            <div
+              onClick={e => {
+                // list.push(list.length);
+                this.setState({
+                  list: [...list, list.length]
+                });
+              }}
+              style={{
+                display: "block",
+                padding: 20,
+                backgroundColor: "teal",
+                margin: 20,
+                color: "white"
+              }}
+              key={i}
+            >
+              {v}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+// On clicking these div, the new div will appear on click
+// The reason is -
+// - In class based component by default react component is not goint to check
+// old state and old props is same to next state and next prop
+// - It is never going to check this state
+// Can can behave like previos function component by shoulComponentUpdate()
+// by list: [...list, list.length]
+// this is immutable again
